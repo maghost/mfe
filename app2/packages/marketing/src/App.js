eadd235edd2ccc60c1550634307dea59b9db409d
Matheus Marques <matheus_maghost@hotmail.com>
2021-01-07 13:19:20 -0300

    feat(app2): create container initial structure

--- app2/packages/marketing/src/App.js
+++ app2/packages/marketing/src/App.js
@@ -6,7 +6,7 @@ import { StylesProvider } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-export default () => {
+const App = () => {
   return (
     <div>
       <StylesProvider>
@@ -20,3 +20,5 @@ export default () => {
     </div>
   );
 };
+
+export default App;
