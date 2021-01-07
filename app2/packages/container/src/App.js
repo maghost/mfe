3562614ee2be40cb889c41b7e18296d60d02cf96
Matheus Marques <matheus_maghost@hotmail.com>
2021-01-07 13:33:22 -0300

    feat(app2): integrate container and marketing mfe

--- app2/packages/container/src/App.js
+++ app2/packages/container/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { mount as mountMarketing } from 'marketing/MarketingApp';
 
 const App = () => {
-  return <h1>Container</h1>;
+  useEffect(() => mountMarketing(document.getElementById('my-marketing')), []);
+
+  return (
+    <>
+      <h1>Container</h1>
+      <div id='my-marketing' />
+    </>
+  );
 };
 
 export default App;
