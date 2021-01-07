65f964a5c9bee915543000f8ad0d2b4e1ee1c9c6
Matheus Marques <matheus_maghost@hotmail.com>
2021-01-06 23:25:07 -0300

    refactor(products): exposes bootstrap instead of index file

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       name: 'products',
       filename: 'remoteEntry.js',
       exposes: {
-        './ProductsIndex': './src/index',
+        './ProductList': './src/bootstrap',
       },
       shared: ['faker/locale/pt_BR'],
     }),
