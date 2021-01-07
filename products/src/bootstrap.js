e57edd06480f08f3136a34ccda14609fb8ce756b
Matheus Marques <matheus_maghost@hotmail.com>
2021-01-06 23:16:38 -0300

    refactor(product): remove direct dependency on an id in the container project

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,12 +1,27 @@
 import faker from 'faker/locale/pt_BR';
 
-let products = '<ul>';
+const mount = (el) => {
+  let products = '<ul>';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<li>${name}</li>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<li>${name}</li>`;
+  }
+
+  products += '</ul>';
+
+  el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.getElementById('dev-products');
+
+  // Assuming our container doesn't have an element
+  // with id 'dev-products'
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
 }
 
-products += '</ul>';
-
-document.getElementById('dev-products').innerHTML = products;
+export { mount };
