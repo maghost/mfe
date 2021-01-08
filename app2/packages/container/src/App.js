df2f52edbb10a39767125e56ba92ea73005e791f
Matheus Marques <matheus_maghost@hotmail.com>
2021-01-08 15:53:35 -0300

    fix(app2): generate a custom classname to styles of material ui

--- app2/packages/container/src/App.js
+++ app2/packages/container/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 // COMPONENTS
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'container',
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
